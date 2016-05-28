f800755fd68bc19fb7613943c0d111167c0f6c0e
songygit <songy@bc-info.net>
2016-05-28 10:07:54 +0800

    commit to local repository first

---
+++ src/com/mottsdk/example/MyApp.java
@@ -0,0 +1,11 @@
+package com.mottsdk.example;
+
+import com.mobileott.util.MobileOTTApplication;
+
+public class MyApp extends MobileOTTApplication {
+
+	public MyApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
