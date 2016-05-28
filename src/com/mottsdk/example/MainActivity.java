e14f2dcfc25c2332bf2d7f9a2369e8b92aff5b64
songy <songy@bc-info.net>
2016-05-28 10:36:27 +0800

    test 2222

--- src/com/mottsdk/example/MainActivity.java
+++ src/com/mottsdk/example/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity implements View.OnClickListener
 
 		myAppUserId.setText(xxStr);
        
-		String xxx = "songyssss";
+		String xxx = "songysssstest222";
 		
 	}
 
